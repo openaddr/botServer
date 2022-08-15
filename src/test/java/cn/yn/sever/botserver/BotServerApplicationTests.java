97fa9331f80bff7bdaccc979fb918d0b433a56fb
withyyy <2413823382>
2022-08-15 21:02:37 +0800

    w

--- src/test/java/cn/yn/sever/botserver/BotServerApplicationTests.java
+++ src/test/java/cn/yn/sever/botserver/BotServerApplicationTests.java
@@ -1,8 +1,12 @@
 package cn.yn.sever.botserver;
 
 import cn.hutool.core.lang.Dict;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import cn.hutool.setting.yaml.YamlUtil;
+import cn.hutool.system.OsInfo;
+import cn.hutool.system.SystemUtil;
 import cn.yn.sever.botserver.entity.YamlEntity;
 import cn.yn.sever.botserver.theConst.GlobalConst;
 import net.mamoe.mirai.Bot;
@@ -14,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -50,15 +55,41 @@ class BotServerApplicationTests {
 
 
     @Test
-    void t1(){
+    void t1() {
         YamlEntity yaml = YamlUtil.loadByPath("config.yml", YamlEntity.class);
         System.out.println(yaml);
 
     }
 
     @Test
-    void t2(){
+    void t2() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("verifyKey", "j5sNJbVdcPaySZt7");
+        String post = HttpUtil.post("http://localhost:4040/verify", JSONUtil.toJsonStr(map));
+        System.out.println(post);
+    }
 
+    @Test
+    void t3(){
+        Map<String, Object> ma = new HashMap<>();
+        ma.put("verifyKey", "j5sNJbVdcPaySZt7");
+        String post = HttpUtil.post("http://localhost:4040/verify", JSONUtil.toJsonStr(ma));
+        JSONObject entries = JSONUtil.parseObj(post);
+        Object session1 = entries.get("session");
+        String session = session1.toString();
+        Map<String ,Object> map = new HashMap<>();
+        map.put("sessionKey",session);
+        map.put("qq","2357902824");
+        String post1 = HttpUtil.post("http://localhost:4040/bind", JSONUtil.toJsonStr(map));
+        System.out.println(post1);
+    }
+
+    @Test
+    void t4() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("verifyKey", "j5sNJbVdcPaySZt7");
+        String post = HttpUtil.post("http://localhost:4040/verify", JSONUtil.toJsonStr(map));
+        System.out.println(post);
     }
 
 }
